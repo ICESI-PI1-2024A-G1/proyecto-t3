b4ebd88e8471501873e364ddaba45dc18fe7e4dc
Andres Parra <andresdavidparra2002@gmail.com>
2024-03-03 17:05:32 -0500

    feature: añadido formulario de inicio de sesion

--- src/usuarios/views.py
+++ src/usuarios/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_page(request):
+    if request.method == 'POST':
+        form = request.POST
+        login(request, authenticate(request, username=form['username'], password=form['password']))
+        if request.user.is_authenticated:
+            return render(request, 'home.html')
+        
+    return render(request, 'login.html', {
+        'form': AuthenticationForm
+    })
