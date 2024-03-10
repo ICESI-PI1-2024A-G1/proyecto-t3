d9216c94409224f56c169720c5d44d24e53e49e1
Felipe <113157110+rojas435@users.noreply.github.com>
2024-03-09 19:43:17 -0500

    fix: cambio en los urls y cambio de algunos comentarios en el html

--- src/academico/urls.py
+++ src/academico/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("crear-curso", views.crear_curso, name="crear-curso"),
     path("programas", views.programas, name="programas")
+    path("planeacion_materias", views.crear_clase, name="planeacion_materias")
 ]
