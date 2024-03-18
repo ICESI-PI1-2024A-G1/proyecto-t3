7ac7cba57e7433b0faa3648a8bedf05169abb608
RafaelaRuizP <rafaela.ruiz@u.icesi.edu.co>
2024-03-18 09:23:39 -0500

    feat:lista docentes

--- src/usuarios/views.py
+++ src/usuarios/views.py
@@ -1,3 +1,13 @@
+import random
+
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db import IntegrityError
+from django.db.models import Q
+from django.http import HttpResponse
+
+from .models import (TipoContrato, EstadoContrato, EstadoDocente, Contrato, Ciudad,
+                     Persona, Director, Docente)
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import get_object_or_404, redirect, render
@@ -5,7 +15,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from academico.views import programas
 
 from .forms import DocenteForm
-from .models import Docente
+
 
 # Create your views here.
 
@@ -32,3 +42,60 @@ def docente_Detail(request, cedula):
     if request.method=='GET':
         docente = get_object_or_404(Docente, cedula=cedula)
         return render(request, "docenteProfile.html", {'docente': docente})
+    
+#@login_required(login_url="/login")
+def docentes(request):
+    docentes = Docente.objects.all()
+
+    # Búsqueda y filtrado
+    if request.method == "GET":
+        query = request.GET.get("q", None)
+        estado = request.GET.get("estado", None)
+        contrato = request.GET.get("contrato", None)
+        ordenar_por = request.GET.get("ordenar_por", None)
+
+        if query:
+            docentes = docentes.filter(
+                Q(nombre__icontains=query)
+                | Q(cedula__icontains=query)
+            )
+
+        if estado:
+            docentes = docentes.filter(estado__id=estado)
+        if contrato:
+            docentes = docentes.filter(contrato_codigo__tipo_contrato__id=contrato)
+
+        if ordenar_por:
+            docentes = docentes.order_by(ordenar_por)
+
+    paginator = Paginator(docentes, 10) 
+
+    #Paginación
+    
+    page_number = request.GET.get('page')
+    try:
+        docentes = paginator.page(page_number)
+    except PageNotAnInteger:
+        docentes = paginator.page(1)
+    except EmptyPage:
+        docentes = paginator.page(paginator.num_pages)
+
+    estados = EstadoDocente.objects.all()
+    contratos = TipoContrato.objects.all()
+
+    return render(
+        request,
+        "docentes.html",
+        {
+            "docentes": docentes,
+            "estados": estados,
+            "contratos": contratos,
+            "side": "sidebar_principal.html",
+            "side_args": args_principal("docentes"),
+        },
+    )
+
+def args_principal(seleccionado):
+    return {
+        "Docentes posgrado": {"url": "/usuarios/docentes", "seleccionado": seleccionado=="docentes"}
+    }
