44f9e8014db226a1f4aa47362359f77edd8e26ae
Andres Parra <andresdavidparra2002@gmail.com>
2024-03-30 13:04:26 -0500

    feat: sidebar responsive

---
+++ src/academico/static/js/sidebar.js
@@ -0,0 +1,14 @@
+function changeSidebarState() {
+    let sidebar = document.getElementById("page_sidebar_content");
+    let main_content = document.getElementById("page_main_content");
+    let sidebarState = sidebar.style.display;
+    if (sidebarState === "none") {
+        sidebar.style.display = "block";
+        main_content.classList.add("col");
+        main_content.classList.add("pb-3");
+    } else {
+        main_content.classList.remove("col");
+        main_content.classList.remove("pb-3");
+        sidebar.style.display = "none";
+    }
+}
