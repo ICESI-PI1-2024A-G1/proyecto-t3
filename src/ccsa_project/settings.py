32e57ee1fc0adbc41604865f88d4634e387f0502
juanex2001 <juanseg201@gmail.com>
2024-04-14 13:38:13 -0500

    fix(ui): reponiendo settings para emails

--- src/ccsa_project/settings.py
+++ src/ccsa_project/settings.py
@@ -128,3 +128,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'juanseg201@gmail.com' #Email que va a mandar las notificaciones
+EMAIL_HOST_PASSWORD = '----' #Password de Google Authenticator
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True  # Or False if you're using SSL
