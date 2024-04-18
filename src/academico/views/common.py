01748ede61357cc49a36f04ead1c342e65e566be
Alejandra <alejamantillac@gmail.com>
2024-04-18 16:19:20 -0500

    feat: pagina de inicio añadida

--- src/academico/views/common.py
+++ src/academico/views/common.py
@@ -1,6 +1,13 @@
+import json
 import random
 
-from academico.models import Clase
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count
+from django.db.models.functions import ExtractWeekDay
+from django.shortcuts import render
+
+from academico.models import Clase, Materia, Programa
+from usuarios.models import Docente
 
 
 def args_principal(seleccionado):
@@ -14,6 +21,7 @@ def args_principal(seleccionado):
         dict: Un diccionario con la información de los menús, donde la clave es el nombre del menú y el valor es otro diccionario con la URL y si está seleccionado o no.
     """
     return {
+        "Inicio": {"url": "/academico/inicio", "seleccionado": seleccionado=="Inicio"},
         "Programas posgrado": {"url": "/academico/programas", "seleccionado": seleccionado=="programas"},
         "Materias posgrado": {"url": "/academico/materias", "seleccionado": seleccionado=="materias"},
         "Docentes posgrado": {"url": "/docentes", "seleccionado": seleccionado=="docentes"},
@@ -52,3 +60,25 @@ def obtener_modalidad(malla):
         except:
             continue
     return max(set(modalidades), key=modalidades.count)
+
+@login_required(login_url="/login")
+def inicio(request):
+    total_programas = Programa.objects.count()
+    total_docentes = Docente.objects.count()
+    total_materias = Materia.objects.count()
+    total_clases = Clase.objects.count()
+
+    estados_programas = list(Programa.objects.values('estado_solicitud__nombre').annotate(total=Count('estado_solicitud')))
+    clases_por_dia = list(Clase.objects.annotate(dia_semana=ExtractWeekDay('fecha_inicio')).values('dia_semana').annotate(total=Count('id')))
+
+    return render(request, "inicio.html", {
+        "total_programas": total_programas,
+        "total_docentes": total_docentes,
+        "total_materias": total_materias,
+        "total_clases": total_clases,
+        "estados_programas_json": json.dumps(estados_programas),
+        "clases_por_dia_json": json.dumps(clases_por_dia),
+        "side": "sidebar_principal.html",
+        "side_args": args_principal("Inicio"),
+    },
+)
