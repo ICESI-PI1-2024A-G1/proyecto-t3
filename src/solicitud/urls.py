c23bdd468804001448100fa29a4cdfbfed0e6bdf
RafaelaRuizP <rafaela.ruiz@u.icesi.edu.co>
2024-04-27 22:24:43 -0500

    feat: organizando merge

--- src/solicitud/urls.py
+++ src/solicitud/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path("viaticos", views.viaticos, name="viaticos")
+    path("viaticos", views.viaticos, name="viaticos"),
     path("crear_viatico", views.solicitud_viaticos, name="crear_viatico"),
     path("salones_solicitud", views.salones_solicitud, name="salones_solicitud"),
     path("asignar_espacio/<int:solicitud_id>/", views.asignar_espacio, name="asignar_espacio"),
--- src/solicitud/views.py
+++ src/solicitud/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import random
 
 from django.contrib import messages
