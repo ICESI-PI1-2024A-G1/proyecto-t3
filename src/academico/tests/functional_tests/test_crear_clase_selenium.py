fd164fbb7e0164dcd26374cfdd9db4db357498af
Felipe <113157110+rojas435@users.noreply.github.com>
2024-05-07 16:18:26 -0500

    feat: Test de clases selenium completado
    Se corrigen para que las pruebas pasen de forma secuencial

--- src/academico/tests/functional_tests/test_crear_clase_selenium.py
+++ src/academico/tests/functional_tests/test_crear_clase_selenium.py
@@ -62,6 +62,7 @@ class LoginPageTestCase(BaseTestCase):
         departamento = Departamento.objects.create(codigo=1, nombre="Departamento 1")
         tipo_materia = TipoDeMateria.objects.create(tipo="1")
         materia = Materia.objects.create(codigo=1, nombre="Materia", creditos=3, departamento=departamento, tipo_de_materia=tipo_materia)
+        
 
         curso = Curso.objects.create(grupo = '4', cupo = 10, materia_id=materia.codigo, periodo_id=periodo.semestre)
         curso = Curso.objects.create(grupo = '5', cupo = 10, materia_id=materia.codigo, periodo_id=periodo.semestre)
@@ -73,6 +74,8 @@ class LoginPageTestCase(BaseTestCase):
         tipo_espacio = Espacio.objects.create(id=4,tipo="Salon", capacidad="5")
 
         modalidad = Modalidad.objects.create(id=1, metodologia="Presencial")
+        modalidad = Modalidad.objects.create(id=3, metodologia="Presencial")
+        modalidad = Modalidad.objects.create(id=4, metodologia="Presencial")
 
 
         ciudad = Ciudad.objects.create(id =100, ciudad="Boyaca")
@@ -127,4 +130,303 @@ class LoginPageTestCase(BaseTestCase):
             self.selenium.current_url, self.live_server_url + "/academico/cursos/1"
         )
         self.assertIn("Clase 1", self.selenium.page_source)
+
+
+    def test_crear_clase_2(self):
+    # Iniciar sesión primero
+        self.selenium.get(self.live_server_url)
+        self.selenium.find_element(By.NAME, "username").send_keys("user")
+        self.selenium.find_element(By.NAME, "password").send_keys("user")
+        self.selenium.find_element(By.ID, "submit").click()
+        self.como_lider()
+
+
+        # Navegar a la página de creación de clase
+        self.selenium.get(self.live_server_url + '/academico/materias')
+        materias = self.selenium.find_elements(By.CSS_SELECTOR, "tbody tr")
+        materias[0].click()
+        
+        
+        curso = self.selenium.find_element(By.ID, "4")
+        
+        curso.click()
+       
+
+        self.selenium.find_element(By.CSS_SELECTOR, "a[onclick=\"show()\"]").click()
+        
+
+        
+        self.selenium.find_element(By.NAME, "start_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "start_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "start_day").send_keys("1600PM")
+
+        self.selenium.find_element(By.NAME, "end_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "end_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "end_day").send_keys("1800PM")
+
+        self.selenium.find_element(By.NAME, "tipo_espacio").send_keys(1)
+
+        self.selenium.find_element(By.NAME, "modalidad_clase").send_keys(1)
+        self.selenium.find_element(By.NAME, "docente_clase").send_keys("juan")
+        
+
+        
+
+        # Hacer clic en el botón de envío
+        self.selenium.find_element(By.CSS_SELECTOR, "button.btn.btn-primary").click()
+
+        
+
+        self.assertEqual(
+            self.selenium.current_url, self.live_server_url + "/academico/cursos/4"
+        )
+        self.assertIn("Modulo de clases 1", self.selenium.page_source)
+        
+    
+
+    
+    def test_crear_clase_3(self):
+        self.selenium.get(self.live_server_url + '/login')
+        self.selenium.find_element(By.NAME, "username").send_keys("user")
+        self.selenium.find_element(By.NAME, "password").send_keys("user")
+        self.selenium.find_element(By.ID, "submit").click()
+        self.como_lider()
+
+        # Navegar a la página de creación de clase
+        self.selenium.get(self.live_server_url + '/academico/materias')
+        materias = self.selenium.find_elements(By.CSS_SELECTOR, "tbody tr")
+        materias[0].click()
+        
+        
+        curso = self.selenium.find_element(By.ID, "9")
+        curso.click()
+
+        self.selenium.find_element(By.CSS_SELECTOR, "a[onclick=\"show()\"]").click()
+
+        self.selenium.find_element(By.NAME, "start_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "start_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "start_day").send_keys("1600PM")
+
+        self.selenium.find_element(By.NAME, "end_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "end_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "end_day").send_keys("1800PM")
+
+        self.selenium.find_element(By.NAME, "tipo_espacio").send_keys(3)
+
+        self.selenium.find_element(By.NAME, "modalidad_clase").send_keys(1)
+        self.selenium.find_element(By.NAME, "docente_clase").send_keys("")
+        self.selenium.find_element(By.NAME, "num_semanas").send_keys(16)
+
+        # Hacer clic en el botón de envío
+        self.selenium.find_element(By.CSS_SELECTOR, "button.btn.btn-primary").click()
+
+        self.assertEqual(
+            self.selenium.current_url, self.live_server_url + "/academico/cursos/9"
+        )
+        self.assertIn("Clase 1", "Clase 16", self.selenium.page_source)
+    
+    
+    def test_crear_clase_4(self):
+        self.selenium.get(self.live_server_url + '/login')
+        self.selenium.find_element(By.NAME, "username").send_keys("user")
+        self.selenium.find_element(By.NAME, "password").send_keys("user")
+        self.selenium.find_element(By.ID, "submit").click()
+        self.como_lider()
+
+        # Navegar a la página de creación de clase
+        self.selenium.get(self.live_server_url + '/academico/materias')
+        materias = self.selenium.find_elements(By.CSS_SELECTOR, "tbody tr")
+        materias[0].click()
+        
+
+        curso = self.selenium.find_element(By.ID, "10")
+        curso.click()
+
+        self.selenium.find_element(By.CSS_SELECTOR, "a[onclick=\"show()\"]").click()
+
+        self.selenium.find_element(By.NAME, "start_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "start_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "start_day").send_keys("1600PM")
+
+        self.selenium.find_element(By.NAME, "end_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "end_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "end_day").send_keys("1800PM")
+
+        self.selenium.find_element(By.NAME, "tipo_espacio").send_keys(1)
+
+        self.selenium.find_element(By.NAME, "modalidad_clase").send_keys(1)
+        self.selenium.find_element(By.NAME, "docente_clase").send_keys("juan")
+
+        # Hacer clic en el botón de envío
+        self.selenium.find_element(By.CSS_SELECTOR, "button.btn.btn-primary").click()
+
+        self.assertEqual(
+            self.selenium.current_url, self.live_server_url + "/academico/cursos/10"
+        )
+        self.assertIn("Clase 1", self.selenium.page_source)
+
+    
+    def test_crear_clase_5(self):
+        self.selenium.get(self.live_server_url + '/login')
+        self.selenium.find_element(By.NAME, "username").send_keys("user")
+        self.selenium.find_element(By.NAME, "password").send_keys("user")
+        self.selenium.find_element(By.ID, "submit").click()
+        self.como_lider()
+
+        # Navegar a la página de creación de clase
+        self.selenium.get(self.live_server_url + '/academico/materias')
+        materias = self.selenium.find_elements(By.CSS_SELECTOR, "tbody tr")
+        materias[0].click()
+        
+        
+        curso = self.selenium.find_element(By.ID, "13")
+        curso.click()
+
+        self.selenium.find_element(By.CSS_SELECTOR, "a[onclick=\"show()\"]").click()
+
+        self.selenium.find_element(By.NAME, "start_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "start_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "start_day").send_keys("1600PM")
+
+        self.selenium.find_element(By.NAME, "end_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "end_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "end_day").send_keys("1800PM")
+
+        self.selenium.find_element(By.NAME, "tipo_espacio").send_keys(1)
+
+        self.selenium.find_element(By.NAME, "modalidad_clase").send_keys(1)
+        self.selenium.find_element(By.NAME, "docente_clase").send_keys("juan")
+
+        # Hacer clic en el botón de envío
+        self.selenium.find_element(By.CSS_SELECTOR, "button.btn.btn-primary").click()
+
+        self.assertEqual(
+            self.selenium.current_url, self.live_server_url + "/academico/cursos/13"
+        )
+        self.assertIn("Clase 1", self.selenium.page_source)
+
+    
+    def test_crear_clase_6(self):
+        self.selenium.get(self.live_server_url + '/login')
+        self.selenium.find_element(By.NAME, "username").send_keys("user")
+        self.selenium.find_element(By.NAME, "password").send_keys("user")
+        self.selenium.find_element(By.ID, "submit").click()
+        self.como_lider()
+
+        # Navegar a la página de creación de clase
+        self.selenium.get(self.live_server_url + '/academico/materias')
+        materias = self.selenium.find_elements(By.CSS_SELECTOR, "tbody tr")
+        materias[0].click()
+        
+
+        curso = self.selenium.find_element(By.ID, "16")
+        curso.click()
+
+        self.selenium.find_element(By.CSS_SELECTOR, "a[onclick=\"show()\"]").click()
+
+        self.selenium.find_element(By.NAME, "start_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "start_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "start_day").send_keys("1600PM")
+
+        self.selenium.find_element(By.NAME, "end_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "end_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "end_day").send_keys("1800PM")
+
+        self.selenium.find_element(By.NAME, "tipo_espacio").send_keys(1)
+
+        self.selenium.find_element(By.NAME, "modalidad_clase").send_keys(1)
+        self.selenium.find_element(By.NAME, "docente_clase").send_keys("")
+
+        # Hacer clic en el botón de envío
+        self.selenium.find_element(By.CSS_SELECTOR, "button.btn.btn-primary").click()
+
+        self.assertEqual(
+            self.selenium.current_url, self.live_server_url + "/academico/cursos/16"
+        )
+        self.assertIn("Clase 1", self.selenium.page_source)
+    
+    
+    def test_crear_clase_7(self):
+        self.selenium.get(self.live_server_url + '/login')
+        self.selenium.find_element(By.NAME, "username").send_keys("user")
+        self.selenium.find_element(By.NAME, "password").send_keys("user")
+        self.selenium.find_element(By.ID, "submit").click()
+        self.como_lider()
+
+        # Navegar a la página de creación de clase
+        self.selenium.get(self.live_server_url + '/academico/materias')
+        materias = self.selenium.find_elements(By.CSS_SELECTOR, "tbody tr")
+        materias[0].click()
+        
+
+        curso = self.selenium.find_element(By.ID, "19")
+        curso.click()
+
+        self.selenium.find_element(By.CSS_SELECTOR, "a[onclick=\"show()\"]").click()
+
+        self.selenium.find_element(By.NAME, "start_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "start_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "start_day").send_keys("1600PM")
+
+        self.selenium.find_element(By.NAME, "end_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "end_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "end_day").send_keys("1500PM")
+
+        self.selenium.find_element(By.NAME, "tipo_espacio").send_keys(1)
+
+        self.selenium.find_element(By.NAME, "modalidad_clase").send_keys(1)
+        self.selenium.find_element(By.NAME, "docente_clase").send_keys("juan")
+
+        # Hacer clic en el botón de envío
+        self.selenium.find_element(By.CSS_SELECTOR, "button.btn.btn-primary").click()
+
+        self.assertEqual(
+            self.selenium.current_url, self.live_server_url + "/academico/cursos/19"
+        )
+        self.assertIn("La fecha/hora de inicio no puede ser posterior a la fecha/hora de finalización.", self.selenium.page_source)
+
+    
+    def test_crear_clase_8(self):
+        self.selenium.get(self.live_server_url + '/login')
+        self.selenium.find_element(By.NAME, "username").send_keys("user")
+        self.selenium.find_element(By.NAME, "password").send_keys("user")
+        self.selenium.find_element(By.ID, "submit").click()
+        self.como_lider()
+
+        # Navegar a la página de creación de clase
+        self.selenium.get(self.live_server_url + '/academico/materias')
+        materias = self.selenium.find_elements(By.CSS_SELECTOR, "tbody tr")
+        materias[0].click()
+        
+
+        curso = self.selenium.find_element(By.ID, "22")
+        curso.click()
+
+        self.selenium.find_element(By.CSS_SELECTOR, "a[onclick=\"show()\"]").click()
+
+        self.selenium.find_element(By.NAME, "start_day").send_keys("02/20/2024")
+        self.selenium.find_element(By.NAME, "start_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "start_day").send_keys("1600PM")
+
+        self.selenium.find_element(By.NAME, "end_day").send_keys("02/22/2024")
+        self.selenium.find_element(By.NAME, "end_day").send_keys(Keys.TAB)
+        self.selenium.find_element(By.NAME, "end_day").send_keys("1800PM")
+
+        self.selenium.find_element(By.NAME, "tipo_espacio").send_keys(1)
+
+        self.selenium.find_element(By.NAME, "modalidad_clase").send_keys(1)
+        self.selenium.find_element(By.NAME, "docente_clase").send_keys("")
+
+        # Hacer clic en el botón de envío
+        self.selenium.find_element(By.CSS_SELECTOR, "button.btn.btn-primary").click()
+
+        self.assertEqual(
+            self.selenium.current_url, self.live_server_url + "/academico/cursos/22"
+        )
+        self.assertIn("La duración de la clase no puede ser mayor a 24 horas", self.selenium.page_source)    
+    
+
+
+
+
     
