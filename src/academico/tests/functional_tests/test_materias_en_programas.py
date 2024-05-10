7b783736acd20542ce89c3976162f2f6b3bd39a0
juanex2001 <juanseg201@gmail.com>
2024-05-09 22:52:00 -0500

    fix(ui): Pruebas materias en programa selenium

--- src/academico/tests/functional_tests/test_materias_en_programas.py
+++ src/academico/tests/functional_tests/test_materias_en_programas.py
@@ -6,15 +6,10 @@ from django_selenium_test import PageElement, SeleniumTestCase
 from mixer.backend.django import mixer
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select, WebDriverWait
 
-from academico.models import (Departamento, Director, EstadoSolicitud,
-                              Facultad, MallaCurricular, Materia, Periodo,
-                              Programa, TipoDeMateria, TipoDePrograma)
-from usuarios.models import Ciudad, Persona, Usuario
+from usuarios.models import Persona, Usuario, Ciudad
+from academico.models import Programa, Periodo, Materia, Departamento, TipoDeMateria, Director, EstadoSolicitud, Facultad, TipoDePrograma, MallaCurricular
 from usuarios.tests.functional_tests.base import BaseTestCase
-
 
 @skipUnless(getattr(settings, "SELENIUM_WEBDRIVERS", False), "Selenium is unconfigured")
 class MateriasTestCase(BaseTestCase):
@@ -24,95 +19,80 @@ class MateriasTestCase(BaseTestCase):
     submit = PageElement(By.ID, "submit")
     logout = PageElement(By.ID, "logout-btn")
     lista_materias = PageElement(By.ID, "Materias posgrado btn")
-    filtrar_por_programa = PageElement(By.ID, "filtrar_por_programa")
-    filtrar_btn = PageElement(By.CSS_SELECTOR, 'button[type="submit"]')
+    search = PageElement(By.ID, "q")
+    filtro_submit = PageElement(By.ID, "filtrar_materias")
 
     def setUp(self):
-        self.user = User.objects.create_user("user", "user@example.com", "user")
-        grupo = mixer.blend("auth.Group", name="gestores")
-        self.user.groups.add(grupo)
-        self.user.save()
-        
-        persona = mixer.blend(Persona)
-        mixer.blend(Usuario, persona=persona, usuario=self.user)
-        
-        self.departamento = Departamento.objects.create(codigo=1, nombre="Departamento 1")
-        self.departamento2 = Departamento.objects.create(codigo=2, nombre="Departamento 2")
-        self.tipo_materia = TipoDeMateria.objects.create(tipo="1")
-        self.periodo = Periodo.objects.create(semestre="202101", fecha_inicio="2021-01-01", fecha_fin="2021-06-30")
-        
-        self.cali = Ciudad.objects.create(ciudad="Cali")
-        self.facultadA = Facultad.objects.create(nombre="Facultad A")
-        self.director = Director.objects.create(
-            cedula="123456",
-            nombre="juan",
-            email="juan@gmail.com",
-            telefono="123456",
-            ciudad=self.cali,
-            fechaNacimiento="2021-01-01",
-            oficina="oficina",
-        )
-        self.aprobado = EstadoSolicitud.objects.create(nombre="Aprobado")
-        self.maestria = TipoDePrograma.objects.create(nombre="Maestria")
-        self.programa = Programa.objects.create(
-            codigo="P1",
-            nombre="Programa 1",
-            facultad=self.facultadA,
-            director=self.director,
-            estado_solicitud=self.aprobado,
-            tipo_de_programa=self.maestria,
-        )
-        
-        Programa.objects.create(
-            codigo="P2",
-            nombre="Programa 2",
-            facultad=self.facultadA,
-            director=self.director,
-            estado_solicitud=self.aprobado,
-            tipo_de_programa=self.maestria,
-        )
-        
-        self.programa3 = Programa.objects.create(
-            codigo="P3",
-            nombre="Programa 3",
-            facultad=self.facultadA,
-            director=self.director,
-            estado_solicitud=self.aprobado,
-            tipo_de_programa=self.maestria,
-        )
-        
-        self.materia = Materia.objects.create(codigo=1, nombre="Materia 1", creditos=3, departamento=self.departamento, tipo_de_materia=self.tipo_materia)
-        self.materia2 = Materia.objects.create(codigo=2, nombre="Materia 2", creditos=4, departamento=self.departamento2, tipo_de_materia=self.tipo_materia)
-        
-        MallaCurricular.objects.create(materia = self.materia, programa = self.programa, periodo = self.periodo, semestre="202101")
-        MallaCurricular.objects.create(materia = self.materia2, programa = self.programa3, periodo = self.periodo, semestre="202101")
-        
+        self.setup_data5()
+        self.create_user()
+        self.login()
         
 
         
 
     def test_filtrado_positivo_lista_filtrada(self):
-        # Visitar la página
-        self.selenium.get(self.live_server_url)
-
-        # Llenar el formulario y enviar
-        self.username.send_keys("user")
-        self.password.send_keys("user")
-        self.submit.click()
+        self.como_lider()
         
         # Buscar listado de sidebar
+        self.wait_for_element(By.ID, "Materias posgrado btn")
         self.lista_materias.click()
         
-        self.filtrar_por_programa.click()
+        #Llenar form para filrado
+        self.wait_for_element(By.ID, "q")
+        self.search.send_keys(self.initial_db["departamento_1"].nombre)
         
-         # Seleccionar el programa correcto de la lista desplegable
-        Select(self.filtrar_por_programa).select_by_index(1)
-        
-        self.filtrar_btn.click()
-        
+        # Press Enter key
+        self.wait_for_element(By.ID, "q")
+        self.search.send_keys(Keys.ENTER)
 
         # Validar que se redirigió a la página de inicio
         self.assertEqual(
-            self.selenium.current_url, self.live_server_url + "/academico/materias?programa=P1"
+            self.selenium.current_url, self.live_server_url + "/academico/materias?q=Departamento+1"
         )
-        self.assertNotIn("Materia 2", self.selenium.page_source)
+        self.assertNotIn(self.initial_db["departamento_2"].nombre, self.selenium.page_source)
+        
+    def test_filtrado_positivo_lista_Vacia(self):
+        self.como_lider()
+        
+        # Buscar listado de sidebar
+        self.wait_for_element(By.ID, "Materias posgrado btn")
+        self.lista_materias.click()
+        
+        #Llenar form para filrado
+        self.wait_for_element(By.ID, "q")
+        self.search.send_keys(self.initial_db["departamento_4"].nombre)
+        
+        # Press Enter key
+        self.wait_for_element(By.ID, "q")
+        self.search.send_keys(Keys.ENTER)
+
+        # Validar que se redirigió a la página de inicio
+        self.assertEqual(
+            self.selenium.current_url, self.live_server_url + "/academico/materias?q=Departamento+4"
+        )
+        self.assertNotIn(self.initial_db["departamento_1"].nombre, self.selenium.page_source)
+        self.assertNotIn(self.initial_db["departamento_2"].nombre, self.selenium.page_source)
+        
+    def test_filtrado_positivo_no_filtrada(self):
+        self.como_lider()
+        
+        # Buscar listado de sidebar
+        self.wait_for_element(By.ID, "Materias posgrado btn")
+        self.lista_materias.click()
+        
+        #Llenar form para filrado
+        self.wait_for_element(By.ID, "q")
+        self.search.send_keys("")
+        
+        # Press Enter key
+        self.wait_for_element(By.ID, "q")
+        self.search.send_keys(Keys.ENTER)
+
+        # Validar que se redirigió a la página de inicio
+        self.assertEqual(
+            self.selenium.current_url, self.live_server_url + "/academico/materias?q="
+        )
+        self.assertIn(self.initial_db["departamento_1"].nombre, self.selenium.page_source)
+        self.assertIn(self.initial_db["departamento_2"].nombre, self.selenium.page_source)
+        self.assertIn(self.initial_db["departamento_3"].nombre, self.selenium.page_source)
+        self.assertIn(self.initial_db["departamento_4"].nombre, self.selenium.page_source)
--- src/academico/tests/functional_tests/test_materias_search.py
+++ src/academico/tests/functional_tests/test_materias_search.py
@@ -9,10 +9,10 @@ from selenium.webdriver.common.keys import Keys
 
 from usuarios.models import Persona, Usuario, Ciudad
 from academico.models import Programa, Periodo, Materia, Departamento, TipoDeMateria, Director, EstadoSolicitud, Facultad, TipoDePrograma, MallaCurricular
-
+from usuarios.tests.functional_tests.base import BaseTestCase
 
 @skipUnless(getattr(settings, "SELENIUM_WEBDRIVERS", False), "Selenium is unconfigured")
-class MateriasTestCase(SeleniumTestCase):
+class MateriasTestCase(BaseTestCase):
 
     username = PageElement(By.ID, "username")
     password = PageElement(By.ID, "password")
@@ -23,121 +23,76 @@ class MateriasTestCase(SeleniumTestCase):
     filtro_submit = PageElement(By.ID, "filtrar_materias")
 
     def setUp(self):
-        self.user = User.objects.create_user("user", "user@example.com", "user")
-        grupo = mixer.blend("auth.Group", name="gestores")
-        self.user.groups.add(grupo)
-        self.user.save()
-        
-        persona = mixer.blend(Persona)
-        mixer.blend(Usuario, persona=persona, usuario=self.user)
-        
-        self.departamento = Departamento.objects.create(codigo=1, nombre="Departamento 1")
-        self.departamento2 = Departamento.objects.create(codigo=2, nombre="Departamento 2")
-        self.tipo_materia = TipoDeMateria.objects.create(tipo="1")
-        self.periodo = Periodo.objects.create(semestre="202101", fecha_inicio="2021-01-01", fecha_fin="2021-06-30")
-        
-        self.cali = Ciudad.objects.create(ciudad="Cali")
-        self.facultadA = Facultad.objects.create(nombre="Facultad A")
-        self.director = Director.objects.create(
-            cedula="123456",
-            nombre="juan",
-            email="juan@gmail.com",
-            telefono="123456",
-            ciudad=self.cali,
-            fechaNacimiento="2021-01-01",
-            oficina="oficina",
-        )
-        self.aprobado = EstadoSolicitud.objects.create(nombre="Aprobado")
-        self.maestria = TipoDePrograma.objects.create(nombre="Maestria")
-        self.programa = Programa.objects.create(
-            codigo="P1",
-            nombre="Programa 1",
-            facultad=self.facultadA,
-            director=self.director,
-            estado_solicitud=self.aprobado,
-            tipo_de_programa=self.maestria,
-        )
-        
-        self.materia = Materia.objects.create(codigo=1, nombre="Materia 1", creditos=3, departamento=self.departamento, tipo_de_materia=self.tipo_materia)
-        self.materia2 = Materia.objects.create(codigo=2, nombre="Materia 2", creditos=4, departamento=self.departamento2, tipo_de_materia=self.tipo_materia)
-        
-        MallaCurricular.objects.create(materia = self.materia, programa = self.programa, periodo = self.periodo, semestre="202101")
-        MallaCurricular.objects.create(materia = self.materia2, programa = self.programa, periodo = self.periodo, semestre="202101")
-        
+        self.setup_data5()
+        self.create_user()
+        self.login()
         
 
         
 
     def test_filtrado_positivo_lista_filtrada(self):
-        # Visitar la página
-        self.selenium.get(self.live_server_url)
-
-        # Llenar el formulario y enviar
-        self.username.send_keys("user")
-        self.password.send_keys("user")
-        self.submit.click()
+        self.como_lider()
         
         # Buscar listado de sidebar
+        self.wait_for_element(By.ID, "Materias posgrado btn")
         self.lista_materias.click()
         
         #Llenar form para filrado
-        self.search.send_keys("Departamento 1")
+        self.wait_for_element(By.ID, "q")
+        self.search.send_keys(self.initial_db["departamento_1"].nombre)
         
         # Press Enter key
+        self.wait_for_element(By.ID, "q")
         self.search.send_keys(Keys.ENTER)
 
         # Validar que se redirigió a la página de inicio
         self.assertEqual(
             self.selenium.current_url, self.live_server_url + "/academico/materias?q=Departamento+1"
         )
-        self.assertNotIn("Departamento 2", self.selenium.page_source)
+        self.assertNotIn(self.initial_db["departamento_2"].nombre, self.selenium.page_source)
         
     def test_filtrado_positivo_lista_Vacia(self):
-        # Visitar la página
-        self.selenium.get(self.live_server_url)
-
-        # Llenar el formulario y enviar
-        self.username.send_keys("user")
-        self.password.send_keys("user")
-        self.submit.click()
+        self.como_lider()
         
         # Buscar listado de sidebar
+        self.wait_for_element(By.ID, "Materias posgrado btn")
         self.lista_materias.click()
         
         #Llenar form para filrado
-        self.search.send_keys("Departamento 3")
+        self.wait_for_element(By.ID, "q")
+        self.search.send_keys(self.initial_db["departamento_4"].nombre)
         
         # Press Enter key
+        self.wait_for_element(By.ID, "q")
         self.search.send_keys(Keys.ENTER)
 
         # Validar que se redirigió a la página de inicio
         self.assertEqual(
-            self.selenium.current_url, self.live_server_url + "/academico/materias?q=Departamento+3"
+            self.selenium.current_url, self.live_server_url + "/academico/materias?q=Departamento+4"
         )
-        self.assertNotIn("Departamento 2", self.selenium.page_source)
-        self.assertNotIn("Departamento 1", self.selenium.page_source)
+        self.assertNotIn(self.initial_db["departamento_1"].nombre, self.selenium.page_source)
+        self.assertNotIn(self.initial_db["departamento_2"].nombre, self.selenium.page_source)
         
     def test_filtrado_positivo_no_filtrada(self):
-        # Visitar la página
-        self.selenium.get(self.live_server_url)
-
-        # Llenar el formulario y enviar
-        self.username.send_keys("user")
-        self.password.send_keys("user")
-        self.submit.click()
+        self.como_lider()
         
         # Buscar listado de sidebar
+        self.wait_for_element(By.ID, "Materias posgrado btn")
         self.lista_materias.click()
         
         #Llenar form para filrado
+        self.wait_for_element(By.ID, "q")
         self.search.send_keys("")
         
         # Press Enter key
+        self.wait_for_element(By.ID, "q")
         self.search.send_keys(Keys.ENTER)
 
         # Validar que se redirigió a la página de inicio
         self.assertEqual(
             self.selenium.current_url, self.live_server_url + "/academico/materias?q="
         )
-        self.assertIn("Departamento 2", self.selenium.page_source)
-        self.assertIn("Departamento 1", self.selenium.page_source)
+        self.assertIn(self.initial_db["departamento_1"].nombre, self.selenium.page_source)
+        self.assertIn(self.initial_db["departamento_2"].nombre, self.selenium.page_source)
+        self.assertIn(self.initial_db["departamento_3"].nombre, self.selenium.page_source)
+        self.assertIn(self.initial_db["departamento_4"].nombre, self.selenium.page_source)
