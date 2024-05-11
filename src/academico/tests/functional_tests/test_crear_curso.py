c327f926cee3c669ef64e7fa5909653fa2948d8d
Felipe <113157110+rojas435@users.noreply.github.com>
2024-05-10 23:15:28 -0500

    fix: Cambios en pruebas de crear curso

--- src/academico/tests/functional_tests/test_crear_curso.py
+++ src/academico/tests/functional_tests/test_crear_curso.py
@@ -1,4 +1,3 @@
-""""
 from datetime import datetime
 from unittest import skipUnless
 
@@ -10,6 +9,7 @@ from mixer.backend.django import mixer
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+import time 
 
 from academico.models import (Clase, Curso, Departamento, Director, Docente,
                               Espacio, EstadoSolicitud, Facultad,
@@ -29,64 +29,23 @@ class LoginPageTestCase(BaseTestCase):
     
 
     def setUp(self):
-
-        self.user = User.objects.create_user("user", "user@example.com", "user")
-        grupo = mixer.blend("auth.Group", name="gestores")
-        self.user.groups.add(grupo)
-        self.user.save()
-        
-        persona = mixer.blend(Persona)
-        mixer.blend(Usuario, persona=persona, usuario=self.user)
-
-
-        cali = Ciudad.objects.create(ciudad="Cali")
-        facultadA = Facultad.objects.create(nombre="Facultad A")
-        director = Director.objects.create(
-        cedula="123456",
-        nombre="juan",
-        email="juan@gmail.com",
-        telefono="123456",
-        ciudad=cali,
-        fechaNacimiento="2021-01-01",
-        oficina="oficina",
-        )
-        aprobado = EstadoSolicitud.objects.create(nombre="Aprobado")
-        maestria = TipoDePrograma.objects.create(nombre="Maestria")
-        programa = Programa.objects.create(
-        codigo="P1",
-        nombre="Programa 1",
-        facultad=facultadA,
-        director=director,
-        estado_solicitud=aprobado,
-        tipo_de_programa=maestria,
-        )
-
-        
-        periodo = Periodo.objects.create(semestre='202401', fecha_inicio=datetime.now(), fecha_fin=datetime.now())
-        departamento = Departamento.objects.create(codigo=1, nombre="Departamento 1")
-        tipo_materia = TipoDeMateria.objects.create(tipo="1")
-        materia = Materia.objects.create(codigo=1, nombre="Materia", creditos=3, departamento=departamento, tipo_de_materia=tipo_materia)
+        self.setup_data5()
+        self.create_user()
+        self.login()
 
 
 
     def test_crear_clase_2(self):
-        self.selenium.get(self.live_server_url + '/login')
-        self.selenium.find_element(By.NAME, "username").send_keys("user")
-        self.selenium.find_element(By.NAME, "password").send_keys("user")
-        self.selenium.find_element(By.ID, "submit").click()
         self.como_lider()
 
             # Navegar a la página de creación de clase
-        self.selenium.get(self.live_server_url + '/academico/materias')
-        materias = self.selenium.find_elements(By.CSS_SELECTOR, "tbody tr")
-        materias[0].click()
-
+        self.selenium.get(self.live_server_url + '/academico/materias/' + str(self.initial_db["materia_1"].codigo) + '/'+self.initial_db["periodo_1"].semestre)
+        time.sleep(1000)
         self.selenium.find_element(By.CSS_SELECTOR, "a[onclick=\"show()\"]").click()
         self.cupos.click()
         self.cupos.send_keys(1)
         self.submit.click()
 
-    """
 
 
         
